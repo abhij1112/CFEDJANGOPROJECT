41330d6abcb3ea4f2de73a2611c12fdd98a32872
abhij1112 <singhabhijeet11212@gmail.com>
2023-12-24 18:05:57 +0530

    vercel setting

--- trydjango/settings.py
+++ trydjango/settings.py
@@ -158,11 +158,10 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
-]
+STATICFILES_DIRS =os.path.join(BASE_DIR, 'static'),
 
-STATIC_ROOT = os.path.join(BASE_DIR / "staticfiles_build",'static') 
+
+STATIC_ROOT = os.path.join(BASE_DIR,"staticfiles_build",'static') 
 
 # MEDIA_ROOT = BASE_DIR / "staticfiles-cdn" / "uploads"
 
